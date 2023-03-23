214797497f3b07ad67ab86fe30e77bfbe3c5ff5b
GannaKov <zlatta2000@gmail.com>
2023-03-24 00:11:42 +0100

    delete, update a post

--- routers/posts.js
+++ routers/posts.js
@@ -7,11 +7,63 @@ const HttpError = require("../helpers/HttpError");
 //-----------------------------
 //create a post
 
-router.post("/", async (req, res) => {
-  const newPost = new Post(req.body);
+router.post("/", async (req, res, next) => {
   try {
-    const savedPost = await newPost.save();
+    // const { _id: owner } = req.user;
+    // console.log(req.user);
+    const savedPost = await Post.create(req.body);
     res.status(200).json(savedPost);
+  } catch (err) {
+    next(err);
+  }
+});
+
+//update a post
+router.put("/:id", async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      const result = await post.updateOne({ $set: req.body });
+      if (!result) {
+        throw HttpError(404, "Not found ");
+      }
+      res.status(200).json("the post has been updated");
+    } else {
+      res.status(403).json("you can update only your post");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+//delete a post
+router.delete("/:id", async (req, res, next) => {
+  console.log("id", req.params.id);
+  try {
+    const post = await Post.findById(req.params.id);
+    console.log(post);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("you can delete only your post");
+    }
+  } catch (err) {
+    console.log("id", req.params.id);
+    next(err);
+  }
+});
+//like / dislike a post
+
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("The post has been liked");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("The post has been disliked");
+    }
   } catch (err) {
     res.status(500).json(err);
   }
--- routers/users.js
+++ routers/users.js
@@ -14,7 +14,7 @@ router.put("/:id", async (req, res, next) => {
         const salt = await bcrypt.genSalt(10);
         req.body.password = await bcrypt.hash(req.body.password, salt);
       } catch (err) {
-        return res.status(500).json(err);
+        next(err);
       }
     }
     try {
