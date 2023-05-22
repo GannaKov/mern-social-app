90eae4eecdc3a34757707c92ee60d7333c1e25ae
GannaKov <zlatta2000@gmail.com>
2023-05-22 22:39:54 +0200

    upload img

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ dotenv.config();
 const userRouter = require("./routers/users");
 const authRouter = require("./routers/auth");
 const postRouter = require("./routers/posts");
+const multer = require("multer");
+const path = require("path");
 // ---------------------
 const { DB_HOST } = process.env;
 
@@ -22,10 +24,38 @@ mongoose
     console.log(error.message);
     process.exit(1);
   });
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+//------------------
+// const uploadDir = path.join(process.cwd(), "uploads");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images"); //uploadDir
+  },
+  filename: (req, file, cb) => {
+    console.log("file", file);
+    console.log("req.body.name", req.body.name);
+    cb(null, file.originalname);
+  },
+  limits: {
+    fileSize: 1048576,
+  },
+});
+const upload = multer({
+  storage: storage,
+});
+
+app.post("/api/upload", upload.single("file"), async (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully");
+  } catch (err) {
+    console.log(err);
+  }
+});
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
